7b1c29d73fdc32f2304aa86d59196bdc73c4e6e1
felipeb <felipeborgaco@hotmail.com>
2024-11-14 16:10:42 -0300

    dc

--- utils/db.js
+++ utils/db.js
@@ -1,25 +1,17 @@
-const http = require("http");
 const { neon } = require("@neondatabase/serverless");
 require('dotenv').config();
 
-const sql = neon(process.env.DATABASE_URL); // Usa a função neon para criar a conexão com o banco
+const sql = neon(process.env.DATABASE_URL);
 
-const requestHandler = async (req, res) => {
+const handler = async (req, res) => {
     try {
         const result = await sql`SELECT version()`;
         const version = result[0]?.version || 'Versão não encontrada';
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.end(version);
+        res.status(200).send(version);
     } catch (error) {
         console.error("Erro na requisição:", error);
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Erro ao obter versão do banco de dados.");
+        res.status(500).send("Erro ao obter versão do banco de dados.");
     }
 };
 
-http.createServer(requestHandler).listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
-});
-
-
-module.exports = { sql };
+module.exports = { sql, handler };
